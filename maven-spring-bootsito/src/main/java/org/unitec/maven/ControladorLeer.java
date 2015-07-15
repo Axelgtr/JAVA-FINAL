0ccf6e53109984df89503781e4b19072299f4a86
T-107 <T-107@PC264>
2015-07-14 21:39:06 -0500

    controller2

--- maven-spring-bootsito/src/main/java/org/unitec/maven/ControladorLeer.java
+++ maven-spring-bootsito/src/main/java/org/unitec/maven/ControladorLeer.java
@@ -2,6 +2,9 @@
 package org.unitec.maven;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -10,8 +13,13 @@ import org.springframework.stereotype.Controller;
 
 
 @Controller
+@RequestMapping("/")
 public class ControladorLeer {
-    
+    //cada servicio es un metodo
+    @RequestMapping(value="/leer", method = RequestMethod.GET, headers = {"Accept=text/html"})
+    @ResponseBody String leer () throws Exception{
+        return "aqui vamos a mostrar el servicio leer que hicimos ya";
+    }
     
     
 }
